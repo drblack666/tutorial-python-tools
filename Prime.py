f726e10068e9ea1e5a076a1f851ecdee708d29f5
drblack666 <32709049+drblack666@users.noreply.github.com>
2019-09-29 18:15:39 +0200

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,14 +1,11 @@
-def isPrime(number):
-    if (number==1):
-        return False
-    elif (number==2):
-        return True
-    else:
-        for x in range(2,number):
-            if(number % x==0):
-                return False
-        return True
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
 
+    return True
+    
+    
 def test_small_2():
     assert isPrime(2) == False
 
